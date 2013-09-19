61f6ccc141bbde0207a19cb937fd032e05694876
Gustav Häger <hager.gustav@gmail.com>
2013-09-19 20:06:22 +0200

    Renamed all module dirs.
    No need to explicitly state they are module dirs, as there are not that many, and no real non-module dirs in /src

--- src/DenseKitchen.cpp
+++ src/DenseKitchen.cpp
@@ -5,11 +5,6 @@ void DenseKitchen::readConfig(std::string path)
     configuration.readConfig(path);
 }
 
-/*
-  OBS: Qt creator (projektets officiella utvecklingsmiljö) stödjer olika färgkodning
-       för variabler som är medlemsvariabler respektive icke medlemsvariabler (som standard).
-       Därav är prefix/postfix för medlemsvariabler onödiga för oss i vårt arbeta och bör slopas?
- */
 void DenseKitchen::start()
 {
     debug.start(this);
