de9a8bdccc8628e121bdd4ca4f496ff79a41b405
Gustav Häger <hager.gustav@gmail.com>
2013-09-23 09:46:56 +0200

    Added separate diagnostic program

--- src/Utilities/Frame.h
+++ src/Utilities/Frame.h
@@ -1,14 +1,16 @@
 #ifndef FRAME_H
 #define FRAME_H
-
+#include <opencv2/core/core.hpp>
 
 
 class Frame{
 public:
-	Frame();
+	Frame(cv::Mat image);
 	~Frame();
 	
 private:
+    cv::Mat image;
+
 };
 
 #endif
