517c3786cfb715f9a8c2a879f865e1197a9db8df
Gustav Häger <hager.gustav@gmail.com>
2013-10-03 16:14:43 +0200

    Added doxygen comments

--- src/Utilities/Abstractalgorithm.hpp
+++ src/Utilities/Abstractalgorithm.hpp
@@ -1,6 +1,12 @@
 #ifndef ABSTRACTALGORITHM_H
 #define ABSTRACTALGORITHM_H
 #include "FrameList.hpp"
+/*! 
+ *  \brief     Base class for pipeline algorithms
+ *  \details   Derive form this class to maintain a simple interface for all algorithms used in the processing pipeline.
+ *  \version   1.0
+ */
+
 
 class AbstractAlgorithm
 {
