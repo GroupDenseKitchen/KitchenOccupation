2a0104ac66e0ae6d425049b6bee49f7fcae925e1
Mattias Tiger <matti166@student.liu.se>
2013-10-10 14:14:57 +0200

    Initial

---
+++ src/ImageProcessing/Tracking.hpp
@@ -0,0 +1,48 @@
+#ifndef IMAGE_PROCESSOR_H
+#define IMAGE_PROCESSOR_H
+
+#include "../Utilities/utilities.hpp"
+#include "../Utilities/FrameList.hpp"
+#include "../Utilities/Abstractalgorithm.hpp"
+
+/*!
+ *  \brief   Image processing contains functionality for the different
+ *           states of image processing required for human detection and tracking.
+ */
+namespace image_processing
+{
+/*!
+ *  \brief     The Image Processor is the interface to the image processing functionality.
+ *  \version   0.1
+ *  \date      2013-10-07
+ */
+class Tracking : public AbstractAlgorithm
+{
+public:
+
+    /*!
+       \brief   Constructor.
+    */
+    Tracking();
+
+    /*!
+       \brief   Destructor.
+    */
+    ~Tracking();
+
+    /*!
+       \brief   Initializer.
+    */
+    bool initialize();
+
+    /*!
+       \brief   Process the current frame (possibly using older frames) by calling
+                image processing algorithms in order.
+    */
+    void process(FrameList frames) override;
+
+private:
+};
+}
+
+#endif
