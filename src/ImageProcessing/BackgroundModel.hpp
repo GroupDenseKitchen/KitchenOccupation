cab65b155c8ace6153766a033bc58b2e9f0fd1cb
Erik Fall <erikfall@gmail.com>
2013-10-10 14:52:51 +0200

    Created backgroundmodel class

---
+++ src/ImageProcessing/BackgroundModel.hpp
@@ -0,0 +1,44 @@
+#ifndef BACKGROUNDMODEL_HPP
+#define BACKGROUNDMODEL_HPP
+
+#include "../Utilities/utilities.hpp"
+#include "../Utilities/FrameList.hpp"
+#include "../Utilities/Abstractalgorithm.hpp"
+
+/*!
+ *  \brief     Short, one-line description.
+ *  \details   Longer, more detailed description of the class and it's purpose.
+ *  \version   0.1.1 // Not sure about this.
+ *  \date      2013-09-26 // Last change (would be neat if this could be synced with git.)
+ *  \bug       Any knowns bugs in this class are listed here
+ *  \warning   Warnings go here (about how to not use the class etc. etc.)
+ */
+
+class BackgroundModel : public AbstractAlgorithm
+{
+public:
+    BackgroundModel();
+    ~BackgroundModel();
+    void process(FrameList frames) override;
+    bool init();
+
+private:
+    cv::BackgroundSubtractorMOG2 bg;
+    int nmixtures;
+    int dilations;
+    int erotions;
+    int history;
+    float backgroundRatio;
+    float varThresholdGen;
+    float fVarInit;
+    float fVarMin;
+    float fVarMax;
+    float fCT;
+    float fTau;
+    bool isShadowDetection;
+
+};
+
+
+
+#endif
