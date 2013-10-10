1fb141c670431418821e6279bf4da9aede5b3a14
Erik Fall <erikfall@gmail.com>
2013-10-10 15:18:30 +0200

    More unbreaking of merge

--- src/Utilities/Frame.cpp
+++ src/Utilities/Frame.cpp
@@ -21,34 +21,6 @@ std::vector<CameraObject> Frame::getCameras(){
 //TODO nontrivialish, verify it works...
 std::vector<std::string> Frame::getRoomIDs(){
 
-    std::vector<std::string> roomIDs;
-
-    for(std::vector<CameraObject>::iterator it = cameras.begin(); it != cameras.end(); ++it) {
-
-        if(std::find(roomIDs.begin(), roomIDs.end(), it->roomID) != roomIDs.end()){
-            roomIDs.push_back(it->roomID);
-        }
-    }
-
-    return roomIDs;
-}
-
-std::vector<cv::Mat> Frame::getRoomImages(std::string roomID){
-
-    std::vector<cv::Mat> roomImages;
-
-    for(std::vector<CameraObject>::iterator it = cameras.begin(); it != cameras.end(); ++it) {
-        if(roomID.compare(it->roomID)){
-            roomImages.push_back(it->rawImage);
-        }
-    }
-    return roomImages;
-}
-
-
-//TODO nontrivialish, verify it works...
-std::vector<std::string> Frame::getRoomIDs(){
-
 	std::vector<std::string> roomIDs;
 
 	for(std::vector<CameraObject>::iterator it = cameras.begin(); it != cameras.end(); ++it) {
