f07cdb566d2302366b59f624eb4ee86a7dea0dfd
Mattias Tiger <matti166@student.liu.se>
2013-10-10 14:14:57 +0200

    Initial

---
+++ src/ImageProcessing/Tracking.cpp
@@ -0,0 +1,21 @@
+#include "ImageProcessor.hpp"
+
+namespace image_processing
+{
+    Tracking::ImageProcessor() {
+
+    }
+
+    Tracking::~ImageProcessor() {
+
+    }
+
+    bool Tracking::initialize() {
+
+        return true;
+    }
+
+    void Tracking::process(FrameList frames) {
+        //TODO stub
+    }
+}
