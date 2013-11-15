7f17fea630bad30f223b4eb9e52c14436ec1761a
Erik Fall <erikfall@gmail.com>
2013-11-15 10:50:22 +0100

    Merge branch 'master' into StillObjects

--- data/CamRec/camRec.cpp
+++ data/CamRec/camRec.cpp
@@ -1,11 +1,73 @@
-#include "CamRecWindow.h"
-#include <QApplication>
+#include <QDebug>
+#include <QTime>
+#include <QElapsedTimer>
 
-int main(int argc, char *argv[])
+#include <opencv2/opencv.hpp>
+
+int main()
 {
-    QApplication a(argc, argv);
-    CamRecWindow w;
-    w.show();
+    std::string fileName = "videoStream";
 
-    return a.exec();
+    QDateTime current = QDateTime::currentDateTime();
+    QTime time = QTime::currentTime();
+    std::string timeStamp = time.toString(Qt::TextDate).toStdString();
+
+    std::cerr << timeStamp << std::endl;
+
+
+    std::string fileExtension = ".avi";
+    std::string writePath = fileName + fileExtension;
+
+    std::cerr << writePath << std::endl;
+
+    double fps = 30;
+    cv::Size size(640,480);
+
+    cv::VideoCapture* camera = new cv::VideoCapture("http://root:pass@169.254.234.112/axis-cgi/mjpg/video.cgi?resolution=640x480&.mjpg");
+    cv::VideoWriter* videoWriter = new cv::VideoWriter(writePath, CV_FOURCC('D','I','V','X'), fps, size) ;
+
+    cv::Mat image;
+    char key;
+
+    bool recording = true;
+    int frames = 0;
+
+    QElapsedTimer timer;
+    timer.start();
+    qint64 workTime;
+
+    while(recording){
+
+        timer.restart();
+
+        camera->read(image);
+        videoWriter->write(image);
+        cv::imshow("", image);
+
+        key = cv::waitKey(5);
+        if(key == 'q'){
+            qDebug() << "escaping";
+            recording = false;
+        }
+
+        frames++;
+        qDebug() << frames;
+
+        workTime = timer.elapsed();
+
+        qDebug() << "WorkTime: " << workTime;
+
+        int delay = 33 - (int)workTime;
+
+        if( delay < 1 ){
+            delay = 1;
+        }
+        cv::waitKey(delay);
+        std::cerr << timer.elapsed() << std::endl;
+    }
+
+    camera->release();
+    videoWriter->release();
+
+    return 0;
 }
