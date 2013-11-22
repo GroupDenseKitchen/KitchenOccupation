6fcb234f0f2ae58fa3031674296d1fd12b9174fe
Nikolaus West <nikolaus.west@me.com>
2013-11-22 18:43:54 +0100

    In the process of cleaning up the personDetectionCode

--- src/ImageProcessing/PersonDetection.cpp
+++ src/ImageProcessing/PersonDetection.cpp
@@ -11,34 +11,19 @@ namespace image_processing
 
     bool PersonDetection::initialize(configuration::ConfigurationManager &settings)
     {
-        //Defaults
-        CONFIG(settings, detectorType, "detectorType", "ORB");
-        CONFIG(settings, descriptorType, "descriptorType", "ORB");
-        CONFIG(settings, matcherType, "matcherType", "BruteForce-Hamming");
+        CONFIG(settings, lowThreshold, "lowThreshold", 100);
+        CONFIG(settings, highThreshold, "highThreshold", 200);
+        CONFIG(settings, kernelSize, "kernelSize", 3);
+        CONFIG(settings, downSamplingFactor, "downSamplingFactor", 4);
+        CONFIG(settings, circleFilterRadiusDifference, "circleFilterRadiusDifference", 4);
 
-
-
-        detector = cv::FeatureDetector::create( detectorType );
-        descriptorExtractor = cv::DescriptorExtractor::create( descriptorType );
-        descriptorMatcher = cv::DescriptorMatcher::create( matcherType );
-
-        currentKeyPoints = new std::vector<cv::KeyPoint>();
-        previousKeyPoints = new std::vector<cv::KeyPoint>();
-
-        cv::namedWindow("Sliders", CV_WINDOW_AUTOSIZE);
-
-        cv::createTrackbar("Min dist", "Sliders", &minDist,400, [](int,void*){});
-        cv::createTrackbar("Low thresh", "Sliders", &lowThreshold,400, [](int,void*){});
-        cv::createTrackbar("High thresh", "Sliders", &highThreshold,400, [](int,void*){});
-       // cv::createTrackbar("Kernel size", "Sliders", &kernelSize,12, [](int,void*){});
-
+        /*  This is left here for tuning purposes
+        cv::namedWindow("Sliders");
+        cv::createTrackbar("Low thresh", "Sliders", &lowThreshold,100, [](int,void*){});
+        cv::createTrackbar("High thresh", "Sliders", &highThreshold,200, [](int,void*){});
+        */
         makeCircleFilters(circleFilters,1);
 
-        if( detector.empty() || descriptorExtractor.empty() || descriptorMatcher.empty()  )
-            {
-                LOG("PersonDetection Error", "Could not create descriptor, extractor or matcher");
-                return false;
-            }
         return true;
     }
 
@@ -52,10 +37,9 @@ namespace image_processing
             }
 
             cv::Mat gray;
-            cvtColor( camera.getImage("rawImage").clone(), gray, CV_RGB2GRAY);
+            cvtColor( camera.getImage("rawImage"), gray, CV_RGB2GRAY);
 
-
-            //Create foreground mask from object rectangles
+            /*//Create foreground mask from object rectangles
             std::vector<Object> objects = camera.getObjects();
             cv::Mat mask = cv::Mat::zeros( gray.rows, gray.cols, CV_8U );
             for (Object & object: objects) {
@@ -65,10 +49,12 @@ namespace image_processing
             cv::Mat croppedGray;
             gray.copyTo( croppedGray,  mask );
 
-            cv::Mat raw = camera.getImage("rawImage");
+
 
             // Reduce noise with a kernel 3x3
-            cv::blur( croppedGray, croppedGray, cv::Size(kernelSize, kernelSize) );
+            cv::blur( croppedGray, croppedGray, cv::Size(kernelSize, kernelSize) );*/
+
+             cv::Mat raw = camera.getImage("rawImage");
 
             // Canny detector
             cv::Mat canny, cannyGray;
@@ -77,7 +63,6 @@ namespace image_processing
             cv::split( raw , channels);
 
             // Downsampling stuff
-            double downSamplingFactor = 4;
             cv::Mat smallRed, smallBlue, smallGreen, smallGray;
             cv::resize(channels[2],smallRed,cv::Size(0,0), 1/downSamplingFactor,1/downSamplingFactor, CV_INTER_AREA);
             cv::resize(channels[1],smallGreen,cv::Size(0,0), 1/downSamplingFactor,1/downSamplingFactor, CV_INTER_AREA);
@@ -114,7 +99,6 @@ namespace image_processing
 
             camera.addImage( "RGB canny", canny );
             camera.addImage( "Gray Canny" ,cannyGray );
-            camera.addImage( "Gray" ,croppedGray );
             cv::namedWindow("Circle correlation");
             cv::namedWindow("Thresholded circle correlation");
             cv::Mat showMap;
@@ -127,77 +111,11 @@ namespace image_processing
         }
     }
 
-    void PersonDetection::calculatePointMatches()
-    {
-        /*if(!camera.hasImage("rawImage"))
-        {
-            LOG("PersonDetection Error", "Image \"rawImage\" not set in current frame!");
-            return;
-        }
-        previousImage = currentImage.clone();
-        cvtColor( camera.getImage("rawImage").clone(), currentImage, CV_RGB2GRAY);
-        if (previousImage.empty() || currentImage.empty() ) {
-            return;
-        }
-
-        previousKeyPoints = currentKeyPoints;
-        currentKeyPoints = new std::vector<cv::KeyPoint>();
-        detector->detect( currentImage, *currentKeyPoints );
-
-        previousDescriptors = currentDescriptors;
-        descriptorExtractor->compute( currentImage, *currentKeyPoints, currentDescriptors );
-
-        if ( previousKeyPoints->empty() ) {
-            return;
-        }
-        std::vector<cv::DMatch> matches;
-        descriptorMatcher->match( previousDescriptors, currentDescriptors, matches );
-
-        cv::Mat debugImage;
-        qDebug() << "Size of previousKeypoints vector" << previousKeyPoints->size();
-        qDebug() << "Size of currentKeypoints vector" << currentKeyPoints->size();
-        cv::drawMatches( previousImage, *previousKeyPoints,
-                         currentImage, *currentKeyPoints,
-                         matches, debugImage,CV_RGB(0, 255, 0), CV_RGB(0, 0, 255) );
-        camera.addImage( "Person Detection Experiments", debugImage );
-        */
-    }
-
-    void PersonDetection::calculateGradientMagnitude(cv::Mat &grayImg, cv::Mat &gradientMag)
-    {
-        int scale = 1;
-        int delta = 0;
-        int ddepth = CV_16S;
-
-        // Generate grad_x and grad_y
-        cv::Mat grad_x, grad_y;
-        cv::Mat abs_grad_x, abs_grad_y;
-
-        // Gradient X
-        //Scharr( rawGray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-        Sobel( grayImg, grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT );
-        convertScaleAbs( grad_x, abs_grad_x );
-
-        // Gradient Y
-        //Scharr( rawGray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-        Sobel( grayImg, grad_y, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT );
-        convertScaleAbs( grad_y, abs_grad_y );
-
-        // Total Gradient (approximate)
-        addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, gradientMag );
-
-    }
-
     void PersonDetection::makeCircleFilters(std::vector<cv::Mat> & filters, int numCircles)
     {
-
-        //std::vector<int> filterSizes = {43,39,35,31};
-        std::vector<int> filterSizes = {35};
-        int deltaRadius = 4;
-        cv::namedWindow("Circle filters", CV_WINDOW_AUTOSIZE);
         filters.clear();
-        for (int i = 0; i < filterSizes.size(); ++i) {
-            int filterSize = filterSizes[i];
+        for (int i = 0; i < circleFilterSizes.size(); ++i) {
+            int filterSize = circleFilterSizes[i];
             int radius = (filterSize-1)/2;
             cv::Point center( radius, radius );
 
@@ -208,10 +126,10 @@ namespace image_processing
             for(; itn != itn_end; ++itn) {
                 negSum += *itn;
             }
-            assert(negSum < 0 );
 
             cv::Mat testPositive = cv::Mat::zeros(filterSize, filterSize, CV_32FC1);
-            cv::circle(testPositive, center, radius-deltaRadius, 1,deltaRadius);
+            cv::circle(testPositive, center, radius-circleFilterRadiusDifference,
+                                                    1 ,circleFilterRadiusDifference);
             float posSum=0;
             cv::MatConstIterator_<float> itp = testPositive.begin<float>(), itp_end = testPositive.end<float>();
             for(; itp != itp_end; ++itp) {
@@ -221,7 +139,8 @@ namespace image_processing
 
             cv::Mat filter = cv::Mat::zeros(filterSize, filterSize, CV_32FC1);
             cv::circle(filter, center, radius, -1,-1);
-            cv::circle(filter, center, radius -deltaRadius , posWeight ,deltaRadius);
+            cv::circle(filter, center, radius -circleFilterRadiusDifference,
+                                        posWeight ,circleFilterRadiusDifference);
             filters.push_back(filter);
         }
     }
--- src/ImageProcessing/PersonDetection.hpp
+++ src/ImageProcessing/PersonDetection.hpp
@@ -26,32 +26,16 @@ public:
     void process(FrameList &frames) override;
 
 private:
-    void calculateGradientMagnitude(cv::Mat & grayImg, cv::Mat & gradientMag);
-    void calculatePointMatches();
     void makeCircleFilters(std::vector<cv::Mat> & filters, int numCircles);
 
-    cv::Ptr<cv::FeatureDetector> detector;
-    cv::Ptr<cv::DescriptorExtractor> descriptorExtractor;
-    cv::Ptr<cv::DescriptorMatcher> descriptorMatcher;
+    int lowThreshold;
+    int highThreshold;
+    int kernelSize;
+    double downSamplingFactor;
 
-    int lowThreshold = 100;
-    int highThreshold = 200;
-    int minDist = 100;
-    int kernelSize = 3;
-
-
-    std::string detectorType;
-    std::string descriptorType;
-    std::string matcherType;
-
-    cv::Mat previousImage;
-    cv::Mat currentImage;
-    cv::Ptr<std::vector<cv::KeyPoint> > previousKeyPoints;
-    cv::Ptr<std::vector<cv::KeyPoint> > currentKeyPoints;
-    cv::Mat previousDescriptors;
-    cv::Mat currentDescriptors;
     std::vector<cv::Mat> circleFilters;
-
+    std::vector<int> circleFilterSizes = {35}; //TODO figure out how to initialize this with CONFIG
+    int circleFilterRadiusDifference;
 };
 
 }
