06269f2613550d8513da167f141d3f6cd8be4812
Mattias Tiger <matti166@student.liu.se>
2013-11-27 09:01:51 +0100

    Cleaner

--- src/ImageProcessing/EntryExitCounter.cpp
+++ src/ImageProcessing/EntryExitCounter.cpp
@@ -91,11 +91,8 @@ void EntryExitCounter::process(FrameList &frames)
 
 bool isInsidePolygon(cv::Mat mask, cv::Point2d point){
     if(point.x >= 0 && point.y >= 0){
-        if(mask.at<cv::Vec3b>(point)[0] == 255){
-            return true;
-        }
+        return mask.at<cv::Vec3b>(point)[0] == 255;
     }
-
     return false;
 }
 
