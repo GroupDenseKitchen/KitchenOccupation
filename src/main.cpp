ce41f38ea8136afe78e486f0f8b63abfbea550f3
MartinSvensson <marsv106@student.liu.se>
2013-12-13 10:03:36 +0100

    Added short DOxygen project description.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,18 @@
 #include <QtCore>
 #include <iostream>
 
+
+/*!
+ *  \mainpage Kichen Occupation
+ *
+ *  \section About
+ *      This software was created as a part of the course Bilder och Grafik (TSBB11) at linköping university.
+ *      The goal of the project was to create a software system for counting people and eventually haveing the system being used to measure
+ *      room usage intensity at the university. The software is designed to be as modular and general as possible in order to allow
+ *      for easy replacement of different system components.
+ *
+ */
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
