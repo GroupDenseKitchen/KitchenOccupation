7747e5ad191158080e9d47c993d610f7866a86eb
Malin <malru103@student.liu.se>
2013-12-13 20:49:11 +0100

    Small doxygen updates

--- src/Utilities/Algorithm.hpp
+++ src/Utilities/Algorithm.hpp
@@ -10,7 +10,6 @@ class AlgorithmFactory;
 /*!
  * \brief     Base class for pipeline algorithms
  * \details   Derive form this class to maintain a simple interface for all algorithms used in the processing pipeline.
- * \version   1.0
  */
 class Algorithm
 {
