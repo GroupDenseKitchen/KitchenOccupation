3683a7149d773e1313cda4c767038df1e46bc2a1
gustav <gustav@ubuntu.(none)>
2013-12-13 15:03:42 +0100

    word

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+#include "Utilities/utilities.hpp"
 #include "Debugging/Logger.hpp"
 #include "DenseKitchen.hpp"
+#include "Utilities/FrameList.hpp"
+#include "Utilities/Frame.hpp"
+#include "Utilities/CameraObject.hpp"
+
 
 #ifndef HEADLESS
   #include "Debugging/MainDebugWindow.hpp"
@@ -7,7 +12,67 @@
   #include <QtCore>
 #endif
 
-#include <iostream>
+cv::FileStorage configFile;
+std::vector<cv::Point> polygon;
+std::vector<std::vector<cv::Point> > doorPolygons;
+std::vector<std::vector<cv::Point> > exclusionPolygons;
+bool drawAsCircle, isDrawingCircle;
+cv::Point circleCenter;
+int circleRadius;
+
+bool readMasks(std::vector<std::vector<cv::Point> > &polygons, std::string nodeName)
+{
+    std::stringstream ss;
+    ss << nodeName << "Count";
+    std::vector<int> numberOfPolygons;
+    configFile[ss.str()] >> numberOfPolygons;
+    std::vector<int> node;
+    cv::Point point;
+
+    for(unsigned int i = 0; i < numberOfPolygons.size(); i++){
+        polygon.clear();
+
+        for(int j = 0; j < numberOfPolygons[i]; j++){
+            ss.str("");
+            ss << nodeName << i << j;
+
+            configFile[ss.str()] >> node;
+
+            if(node.size() == 2){
+                point = cv::Point(node[0], node[1]);
+                polygon.push_back(point);
+            }
+        }
+
+        polygons.push_back(polygon);
+        polygon.clear();
+    }
+
+    return true;
+}
+
+void loadMaskFromFile()
+{
+
+    std::string filePath = "masks.yml";
+    if(configFile.open(filePath, cv::FileStorage::READ)){
+        readMasks(doorPolygons, "doorPolygons");
+        readMasks(exclusionPolygons, "exclusionPolygons");
+        configFile["circleCenterX"] >> circleCenter.x;
+        configFile["circleCenterY"] >> circleCenter.y;
+        configFile["circleRadius"] >> circleRadius;
+    }
+    configFile.release();
+    //sendMasksToFrameList();
+}
+
+/*
+mainProgram->getFrames()->setDoorMask(doorMask);
+mainProgram->getFrames()->setExclusionMask(exclusionMask);
+mainProgram->getFrames()->setCheckPointMaskSmall(checkpointMaskSmall);
+mainProgram->getFrames()->setCheckPointMaskMedium(checkpointMaskMedium);
+mainProgram->getFrames()->setCheckPointMaskLarge(checkpointMaskLarge);
+*/
 
 int main(int argc, char *argv[])
 {
@@ -31,10 +96,29 @@ int main(int argc, char *argv[])
    
    bool iterOK = true;
 
-   while(iterOK){
    	//run the acual program
    	iterOK = dk.singleIteration();
-   	if(iterOK){
+	FrameList* frames = dk.getFrames();
+	cv::Mat fakeMask;
+	if(frames->getCurrent().getCameras().size() > 0){
+
+		fakeMask = frames->getCurrent().
+				   getCameras()[0].
+				   getImage("rawImage").clone();
+		fakeMask.setTo(cv::Scalar(0,0,0));
+	}	
+
+	//checkpoint masks
+	frames->setCheckPointMaskSmall(fakeMask.clone());
+	frames->setCheckPointMaskMedium(fakeMask.clone());
+	frames->setCheckPointMaskLarge(fakeMask.clone());	
+   	frames->setInclusionMask(fakeMask.clone());	
+	frames->setDoorMask(fakeMask.clone());	
+	frames->setExclusionMask(fakeMask.clone());
+
+   while(iterOK){
+	iterOK = dk.singleIteration();
+	if(iterOK){
 		printf("iteration success! \n");
    	}else{
 		printf("error iterating, dying! \n");
